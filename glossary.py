80ee87bdeb210cb017f42d7858c2642a95c9c287
Niam Patel <naz1997@googlemail.com>
2013-05-27 16:58:07 +0100

    added parsing for write functionality

--- glossary.py
+++ glossary.py
@@ -1,7 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
 import csv
-import re
 
 glossary = None
 
@@ -15,7 +14,7 @@ def pull_data(filename):
   raw.close()
 
 def write_data(filename, data):
-  "Takes an object[list/dict] to write to the specified .csv file"
+  "Takes an object[list/dict] to write to the specified .csv file, then calls pull_data()"
   raw = open(filename, "w")
   write = csv.writer(raw)
   raw.close()
@@ -25,43 +24,55 @@ def retreive_entry(event):
   raw = event.widget.get()
   if raw == "?":
     event.widget.delete(0, len(event.widget.get()))
-    event.widget.insert(0, "Place a `+` symbol before the entry to add it to the file. Press <Tab> to clear the field.")
+    event.widget.insert(0, "Type the entry, then Control-n to add it to the file. <Tab> clears the field.")
+  elif raw == "?1":
+    event.widget.delete(0, len(event.widget.get()))
+    event.widget.insert(0, "Type added definitions as 'name:definition'")
   else:
     try:
       data = glossary[raw]
     except:
-      #messagebox.showerror("Err 404","Definition not found :(")
       event.widget.delete(0, len(event.widget.get()))
       event.widget.insert(0, "Oops, looks like that definition couldn't be found!")
     else:
-      #messagebox.showinfo("Meaning:", data)
       event.widget.delete(0, len(event.widget.get()))
       event.widget.insert(0, data)
 
 def add_entry(event):
-  "Adds a new entry into the .csv file"
+  "Parses entry into a list, then calls write_data()"
   raw = event.widget.get()
-
-def parse_entry(event):
-  raw = event.widget.get()
+  data = raw.split(":")
+  data[:] = [x.strip() for x in data]
+  #key = re.search('^\w*', raw)
+  #key = key.group()
+  #value = re.search('', raw)
+  event.widget.delete(0, len(raw))
+  event.widget.insert(0, [d for d in data])
 
 def clear_entry(event):
+  "Clear the target entry box"
   event.widget.delete(0, len(event.widget.get()))
+
+def destroy(event):
+  "A function to facilitate the closing of the window with a command"
+  event.widget.destroy()
 
 def build_widgets(window):
   "Renders and packs the widget elements onto the screen"
-  l1 = Label(window, text = "Enter a term to search, then press Return to execute the query.")
+  l1 = Label(window, text = "Enter a term to search, then press Return to execute the query. ? = help:pg1, ?1 = help:pg2")
   l1.pack(side = TOP)
   e1 = Entry(window, exportselection = 0, justify=CENTER)
   e1.pack(side = BOTTOM, expand = 1, fill = "x")
   e1.bind("<Return>", retreive_entry)
+  e1.bind("<Control-n>", add_entry)
   e1.bind("<Tab>", clear_entry)
 
 app = Tk()
 win = Frame(app)
 win.pack()
+app.bind("<Control-q>", destroy)
 app.title("Glossary")
-app.maxsize(500,50)
+app.maxsize(600,50)
 pull_data("glossary.csv")
 
 build_widgets(win)
